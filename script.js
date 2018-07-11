d8a5e018831a41cd9c114f43a5879158baf7d02b
Yipeechen <yipeechen0418@gmail.com>
2018-07-11 18:51:58 +0800

    ALERT intro: add button to close alert

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(function(){
 
+    //ALERT:點擊紅色close按鈕關閉提示
+    $('.js-close-alert').on('click',function(){
+      $('.alert').alert('close')
+    })
+
+    //關閉提示觸發兩項事件：close.bs.alert & closed.bs.alert
+    $('.alert').on('close.bs.alert', () => {  //執行關閉
+      console.log('close alert')
+    })
+
+    $('.alert').on('closed.bs.alert', () => {  //關閉結束
+      console.log('closed alert')
+    })
 });
