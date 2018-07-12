fb2b2f15ff48142369d19426df26b9ffe8a128df
Yipeechen <yipeechen0418@gmail.com>
2018-07-12 12:16:21 +0800

    CAROUSEL intro:setting interval using js

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ $(function(){
     $('.alert').on('closed.bs.alert', () => {  //關閉結束
       console.log('closed alert')
     })
+
+    //CAROUSEL:預設五秒鐘播放下一張，間隔時間設定method-2(js)
+    $('.carousel').carousel({
+      interval: 3000  //間隔時間
+    })
+
+    //滾動時觸發
+    $('.carousel').on('slide.bs.carousel', function(event){
+      console.log('slide: ', `
+        方向：${ event.direction }
+        從： ${ event.from }
+        到： ${ event.to }
+      `)
+    })
+    //完成滾動後觸發
+    $('.carousel').on('slid.bs.carousel', function(event){
+      console.log('slid: ', `
+        方向：${ event.direction }
+        從： ${ event.from }
+        到： ${ event.to }
+      `)
+    })
 });
